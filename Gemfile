431567caa3eee0644fc4266a1666b71b0133d8fd
Raduh <radu.hambasan@gmail.com>
2014-01-03 01:58:41 +0100

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -28,7 +28,8 @@ end
 gem 'jquery-rails'
 
 group :test do
-  gem 'capybara', '1.1.2'
+  gem 'capybara',           '1.1.2'
+  gem 'factory_girl_rails', '1.4.0'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name                  "Radu Hambasan"
+        email                 "rhambasan@example.com"
+        password              "foobar"
+        password_confirmation "foobar"
+    end
+end
